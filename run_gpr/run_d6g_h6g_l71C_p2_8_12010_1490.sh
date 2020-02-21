e985190f2ebf552eb787fcf2b081d0c8db200d8f
Roger Creel <rcc2167@columbia.edu>
2020-02-21 11:41:27 -0500

    committing output files

---
+++ run_gpr/run_d6g_h6g_l71C_p2_8_12010_1490.sh
@@ -0,0 +1,3 @@
+cd ..
+python -m memory_profiler readv_it.py --mod d6g_h6g_ --lith l71C --um p2 --lm 8 --tmax 12010 --tmin 1490
+exit
---
+++ run_gpr/run_glac1d_l71C_p2_3_10010_990.sh
@@ -0,0 +1,3 @@
+cd ..
+python -m memory_profiler readv_it.py --mod glac1d_ --lith l71C --um p2 --lm 3 --tmax 10010 --tmin 990
+exit
